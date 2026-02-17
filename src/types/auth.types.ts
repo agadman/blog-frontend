8da551f24e7de7fc916888820c303e98c0d2da36
Annika Gadman <annika.gadman@gmail.com>
2026-02-16 23:37:05 -0600

    added auth types

---
+++ src/types/auth.types.ts
@@ -0,0 +1,22 @@
+export interface User {
+  id: string,
+  username: string,
+  email: string,
+  createdAt?: string
+}
+
+export interface LoginCredentials {
+    email: string,
+    password: string
+}
+
+export interface AuthResponse {
+    user: User,
+    token: string
+}
+
+export interface AuthContextType {
+    user: User | null,
+    login: (credentials: LoginCredentials) => Promise<void>;
+    logout: () => void;
+}
